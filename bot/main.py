6a44caaaff8c3d65673716a11c897683cde26db0
mo-ri-regen <moricontb@gmail.com>
2022-11-06 21:37:01 +0900

    ✨Discordにログインしたときに定期的に実行するように修正

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,7 @@
-#00 ライブラリをインポート
+import os
+from dotenv import load_dotenv
+import discord
+
 import schedule
 from time import sleep
 
@@ -9,8 +12,19 @@ def task():
 #02 スケジュール登録
 schedule.every(10).seconds.do(task)
 
+# 接続に必要なオブジェクトを生成
+client = discord.Client(intents=discord.Intents.all())
 
-#03 イベント実行
-while True:
-    schedule.run_pending()
-    sleep(1)
+@client.event
+async def on_ready():
+# 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+    #03 イベント実行
+    while True:
+        schedule.run_pending()
+        sleep(1)
+    
+load_dotenv()
+
+client.run(os.environ['TOKEN'])
