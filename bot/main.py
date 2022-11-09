bcd03099905689982e186feb036b0135d979dd8a
mo-ri-regen <moricontb@gmail.com>
2022-11-10 00:30:11 +0900

    ♻️グローバル変数を使わないように修正

--- bot/main.py
+++ bot/main.py
@@ -8,7 +8,7 @@ import notify_bot
 load_dotenv()
 
 @tasks.loop(seconds=60)
-async def send_message():
+async def send_message(channel_sent):
 
     now = notify_bot.get_now()
 
@@ -31,9 +31,9 @@ client = discord.Client(intents=discord.Intents.all())
 async def on_ready():
 # 起動したらターミナルにログイン通知が表示される
     print('ログインしました')
-    global channel_sent
+    
     channel_sent = client.get_channel(int(os.environ["CHANNEL_ID"]))
 
-    send_message.start() #定期実行するメソッドの後ろに.start()をつける 
+    send_message.start(channel_sent) #定期実行するメソッドの後ろに.start()をつける 
     
 client.run(os.environ['TOKEN'])
